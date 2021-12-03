73ba026a5e8c60431310215a8d1848b614ec9cf4
Chinh Pham <iamdchinh@gmail.com>
2021-12-03 23:39:49 +0700

    modify country code

---
+++ countryCode.go
@@ -0,0 +1,13 @@
+package golyrics
+
+var CountryCode = map[string]string{
+	"en":  "english",
+	"es":  "spanish",
+	"fr":  "french",
+	"de":  "german",
+	"cn":  "chinese-trad",
+	"jp":  "japanese",
+	"jpr": "japanese-romaji",
+	"kp":  "korean",
+	"vi":  "vietnamese",
+}
